6cb7091fc98ea660d3d634324aa9c7daadf139ef
Justin Bell <jbell3e@gmail.com>
2019-10-28 20:39:14 -0500

    customized devise views added name username etc.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_002157) do
+ActiveRecord::Schema.define(version: 2019_10_29_010822) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,10 @@ ActiveRecord::Schema.define(version: 2019_10_29_002157) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "username"
+    t.text "bio"
+    t.string "location"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
