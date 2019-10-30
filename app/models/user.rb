48197cfcc930cb9e4c4e52ccb6d3f9c360481ab7
Justin Bell <jbell3e@gmail.com>
2019-10-30 18:41:46 -0500

    tweet scaffold and associate with the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username, presence: true, uniqueness: true
+
+  has_many :tweets
+  
+  mount_uploader :avatar, AvatarUploader       
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_010822) do
+ActiveRecord::Schema.define(version: 2019_10_29_212944) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "author"
     t.text "blog_entry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tweets", force: :cascade do |t|
+    t.string "message"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -32,6 +39,7 @@ ActiveRecord::Schema.define(version: 2019_10_29_010822) do
     t.string "username"
     t.text "bio"
     t.string "location"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
