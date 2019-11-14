42bdd5aca363162dd36a9f09dc721a9e8ad6ba5f
Justin Bell <jbell3e@gmail.com>
2019-11-13 19:08:50 -0600

    create helpers

---
+++ app/controllers/epicenter_controller.rb
@@ -0,0 +1,32 @@
+class EpicenterController < ApplicationController
+	def tag_tweets
+	@tag = Tag.find(params[:id])
+	end	
+  	def feed
+  		@following_tweets = []
+	
+	Tweet.all.each do |tweet|
+	 	unless current_user.blank?
+	     if current_user.following.include?(tweet.user_id)|| current_user.id == tweet.user_id
+	         @following_tweets.push(tweet)
+	     end
+	 else
+	 	redirect_to new_user_session_path
+	 	return
+	 end
+	 end
+	 end
+	 def show_user
+	     @user = User.find(params[:id])
+	 end
+	 def now_following
+	 	current_user.following.push(params[:id].to_i)
+	 	current_user.save
+	 	redirect_to show_user_path(id: params[:id])
+	 end
+	 def unfollow
+	 	current_user.following.delete(params[:id].to_i)
+	 	current_user.save
+	 	redirect_to show_user_path(id: params[:id])
+	 end
+	end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :validatable
 
   validates :username, presence: true, uniqueness: true
 
   has_many :tweets
-  
+  serialize :following, Array
   mount_uploader :avatar, AvatarUploader       
 end
